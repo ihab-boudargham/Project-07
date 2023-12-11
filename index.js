4107f4e3ca4b1e1659c92d1022c046bd0e69344f
ihab-boudargham <ihab.boudargham@hotmail.com>
2023-12-11 11:18:00 +0200

    AddTimer

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let userPattern = [];
 let correctPattern = [];
 let possibleColors = ["green", "red", "yellow", "blue"];
 let level = 0;
-let highScore = 0;
+let timer; // Timer variable
+let timeLimit = 10000; // Initial time limit in milliseconds
+let timerInterval; // Timer interval variable
 
 // to check everything we are doing is working
 // $(document).keydown(function(){
@@ -24,12 +26,61 @@ let highScore = 0;
         correctPattern.push(color);
         playAudio(color);
         clickAnnimation("#" + color);
+        startTimer();
     }
+
+    function updateTimerDisplay() {
+        document.getElementById('timer-display').textContent = timeLimit / 1000 + 's';
+    }
+
+    function startTimer() {
+        timeLimit = 10000; // Reset time limit for each level
+        updateTimerDisplay();
+    
+        // Clear any existing timer interval
+        clearInterval(timerInterval);
+    
+        // Start the countdown timer
+        timerInterval = setInterval(function () {
+            timeLimit -= 1000; // Decrease time by 1 second
+            updateTimerDisplay(); // You can implement this function to update the UI with the remaining time
+    
+            if (timeLimit <= 0) {
+                resetGame(); // Call a function to handle timeout (user loses)
+            }
+        }, 1000);
+    }
+
+    function resetTimer() {
+        // Reset the timer to the initial time limit
+        clearInterval(timerInterval);
+        timeLimit = 10000; // Reset time limit to 10 seconds (or your desired initial time)
+        updateTimerDisplay(); // You can implement this function to update the UI with the initial time
+    }
+    
 
     function playAudio(color) {
         let relPath = color === 'lose' ? 'sounds/wrong.mp3' : `sounds/${color}.mp3`;
         let audio = new Audio(relPath);
         audio.play();
+    }
+    
+    function resetGame() {
+        clearInterval(timerInterval);
+        updateTimerDisplay();
+        
+        // Reset the UI for a new game
+        $(".buttons").prop("disabled", false);
+        $(".start").prop("disabled", false);
+        $(".buttons .boxColor").show();
+        $(".buttons").removeClass("shrink text-white");
+        updateGameMessage("You ran out of time. Press Start to play again.");
+
+        playAudio('lose');
+        $(".start").show();
+
+        level = 0;
+
     }
 
 
@@ -77,19 +128,23 @@ let highScore = 0;
 
     function checkAnswer(color) {
         userPattern.push(color);
+    
         if (color == correctPattern[numClick]) {
             if (userPattern.length == correctPattern.length) {
                 setTimeout(function () {
                     $(".buttons").removeClass("shrink");
                     userPattern = [];
                     numClick = -1;
+    
                     if (level > 0) {
                         displayWinMessage();
                         setTimeout(function () {
                             nextSequence();
+                            startTimer(); // Start the timer for the next level
                         }, 1000);
                     } else {
                         nextSequence();
+                        startTimer(); // Start the timer for the next level
                     }
                 }, 500);
             }
@@ -97,13 +152,21 @@ let highScore = 0;
             playAudio('lose');
             let gameMessage = document.querySelector('.game-message');
             gameMessage.style.color = 'red';
-            updateGameMessage("Wrong answer! Press Start to play again.");
-
-            // Remove the "shrink" class after a short delay
-            setTimeout(function () {
-                $(".buttons").removeClass("shrink");
-            }, 500);
     
+            // Check if the user ran out of time
+            if (timeLimit <= 0) {
+                updateGameMessage("Game over! You ran out of time. Press Start to play again.");
+            } else {
+                updateGameMessage("Wrong answer! Press Start to play again.");
+    
+                // Remove the "shrink" class after a short delay
+                setTimeout(function () {
+                    $(".buttons").removeClass("shrink");
+                }, 500);
+            }
+    
+            // Reset the game state
+            resetTimer();
             userPattern = [];
             correctPattern = [];
             level = 0;
